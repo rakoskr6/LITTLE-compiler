0d46cfbcb8a9bd3bd74b1609350b31eff5011ce8
Brian Rieder <brian.rieder777@gmail.com>
2016-09-12 10:08:54 -0400

    Copied step1 Micro.java

---
+++ step2code/src/Micro.java
@@ -0,0 +1,29 @@
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.*;
+import java.util.*;
+import java.lang.String;
+
+
+public class Micro {
+
+	public static void main(String []args) throws Exception {
+		ANTLRFileStream fid = new ANTLRFileStream(args[0]);
+		MicroLexer lexer = new MicroLexer(fid);
+		CommonTokenStream tokenStream = new CommonTokenStream(lexer);
+		//System.out.println(tokenStream.getText());
+		//tokenStream.getText();
+		tokenStream.fill();
+		List<Token> tokenList = tokenStream.getTokens();
+
+		String[] tokenType = lexer.getTokenNames();
+
+		for (int i = 0; i < tokenList.size(); i++) {
+			if (tokenList.get(i).getType() != -1) { // end of file token
+				System.out.println("Token Type: " + tokenType[tokenList.get(i).getType()]);
+				System.out.println("Value: " + tokenList.get(i).getText());
+			}
+
+		}
+
+	}
+}
