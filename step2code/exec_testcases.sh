a95e9799f32eadfcf160b071ce2a265b493b1d2c
Brian Rieder <brian.rieder777@gmail.com>
2016-09-17 14:18:46 -0400

    OS semi-agnostic bulk execution script

--- step2code/exec_testcases.sh
+++ step2code/exec_testcases.sh
@@ -3,12 +3,12 @@
 make
 for testfile in `ls -d testcases/input/* | sort -V` ; do
   echo "Testing file: $testfile"
-  if [[ "$OSTYPE" = "cygwin" ]] ; then
+  if [[ $OSTYPE == "cygwin" ]] ; then
     java -cp "classes/;lib/antlr.jar" Micro $testfile
-  elif [[ "$" = "linux-gnu" ]] ; then
-    java -cp classes/;lib/antlr.jar Micro $testfile
-  else 
-    echo "Error: Output of uname does not match a known environment syntax."
+  elif [[ $OSTYPE == "linux-gnu" ]] ; then
+    java -cp classes/:lib/antlr.jar Micro $testfile
+  else
+    echo "Error: Unsupported OSTYPE $OSTYPE"
     exit
   fi
   echo "" # newline
