a19d2b0473fbac2f8dd2f7340c1653ca0e9e77a0
Brian Rieder <brian.rieder777@gmail.com>
2016-09-17 15:29:01 -0400

    Showing expected output of testcases

--- step2code/exec_testcases.sh
+++ step2code/exec_testcases.sh
@@ -3,12 +3,20 @@
 # Date:   17 September 2016
 
 make
-for testfile in `ls -d testcases/input/* | sort -V` ; do
+for testfile in `ls testcases/input/ | sort -V` ; do
+  extstrip=${testfile%.micro}
+  allstrip=${extstrip##*/}
   echo "Testing file: $testfile"
   if [[ $OSTYPE == "cygwin" ]] ; then
-    java -cp "classes/;lib/antlr.jar" Micro $testfile
+    echo -n "Output:   "
+    java -cp "classes/;lib/antlr.jar" Micro testcases/input/$testfile
+    echo -n "Expected: "
+    cat testcases/output/$allstrip.out
   elif [[ $OSTYPE == "linux-gnu" ]] ; then
-    java -cp classes/:lib/antlr.jar Micro $testfile
+    echo -n "Output:   "
+    java -cp classes/:lib/antlr.jar Micro testcases/input/$testfile
+    echo -n "Expected: "
+    cat testcases/output/$allstrip.out
   else
     echo "Error: Unsupported OSTYPE $OSTYPE"
     exit
