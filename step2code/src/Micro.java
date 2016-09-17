30398d8cb6c8a7ae95a88408438d426671354171
Brian Rieder <brian.rieder777@gmail.com>
2016-09-17 16:33:26 -0400

    Throws errors and catches for not accepted, prints for accepted

--- step2code/src/Micro.java
+++ step2code/src/Micro.java
@@ -11,37 +11,14 @@ public class Micro {
 		ANTLRFileStream fid = new ANTLRFileStream(args[0]);
 		MicroLexer lexer = new MicroLexer(fid);
 		CommonTokenStream tokenStream = new CommonTokenStream(lexer);
-		//System.out.println(tokenStream.getText());
-		//tokenStream.getText();
-		
-
-		ByteArrayOutputStream baos = new ByteArrayOutputStream();
-		System.setErr(new PrintStream(baos));
-		String str = baos.toString();
-
-		
-
 		MicroParser parser = new MicroParser(tokenStream);
-		
-		parser.eval();
-		if (str != null && !str.isEmpty())
-		{
-			System.out.println("Fail");
+		try {
+			parser.setErrorHandler(new BailErrorStrategy());
+			parser.eval();
+			System.out.println("Accepted");
+		}	
+		catch (Exception e) {
+			System.out.println("Not Accepted");
 		}
-
-		/*tokenStream.fill();
-		List<Token> tokenList = tokenStream.getTokens();
-
-		String[] tokenType = lexer.getTokenNames();
-
-		for (int i = 0; i < tokenList.size(); i++) {
-			if (tokenList.get(i).getType() != -1) { // end of file token
-				System.out.println("Token Type: " + tokenType[tokenList.get(i).getType()]);
-				System.out.println("Value: " + tokenList.get(i).getText());
-			}
-
-		}
-		*/
-
 	}
 }
