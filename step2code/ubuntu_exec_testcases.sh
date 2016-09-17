6137adabcf399fb558ffc74eb2f24244c917689a
Brian Rieder <brieder@purdue.edu>
2016-09-17 14:19:25 -0400

    Removing extraneous OS-specific execution scripts

--- step2code/ubuntu_exec_testcases.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-make
-for testfile in `ls -d testcases/input/* | sort -V` ; do
-  echo "Testing file: $testfile"
-  java -cp classes/:lib/antlr.jar Micro $testfile
-  echo "" # newline
-done
