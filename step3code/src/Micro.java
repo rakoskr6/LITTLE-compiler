cac69cc726def375e8ccc4171574a33c6ac7ea9d
Brian Rieder <brian.rieder777@gmail.com>
2016-10-05 23:47:10 -0400

    Updated to listen and walk through the program

--- step3code/src/Micro.java
+++ step3code/src/Micro.java
@@ -9,11 +9,23 @@ public class Micro {
 
 	public static void main(String []args) throws Exception {
 		ANTLRFileStream fid = new ANTLRFileStream(args[0]);
-		MicroLexer lexer = new MicroLexer(fid);
-		CommonTokenStream tokenStream = new CommonTokenStream(lexer);
-		MicroParser parser = new MicroParser(tokenStream);
-		
 
+        // Generate the lexer
+		MicroLexer lexer = new MicroLexer(fid);
+
+        // Get a list of matched tokens
+		CommonTokenStream tokenStream = new CommonTokenStream(lexer);
+
+        // Pass the tokens into the parser
+		MicroParser parser = new MicroParser(tokenStream);
+
+        // Specify  our entry point
+		MicroParser.ProgramContext programContext = parser.program();
+
+        // Walk it and attach our listener
+        ParseTreeWalker walker = new ParseTreeWalker();
+        AntlrGlobalListener listener = new AntlrGlobalListener();
+        walker.walk(listener, programContext);
 	}
 
 }
