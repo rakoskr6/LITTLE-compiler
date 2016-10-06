0ad2084c07bb00adae01c052e99f6cff399155cc
Kyle Rakos <krakos@purdue.edu>
2016-10-06 12:23:07 -0400

    Added listeners for needed things and wrote remaining TODO

--- step3code/src/Micro.java
+++ step3code/src/Micro.java
@@ -4,6 +4,14 @@ import java.util.*;
 import java.lang.String;
 import java.io.*;
 
+// DONE
+	// I have added listeners that get us most of the needed info for declarations and scope info
+
+// TODO
+	// Create a symbol table structure.
+	// A new symbol table will be created for each scope then appended to a list
+	// When a variable is declared, it will be added to the last symbol table in the list (ie. the current scope)
+    // When done use Java sets to ensure no duplicate variables within same scope
 
 public class Micro {
 
@@ -27,33 +35,87 @@ public class Micro {
         AntlrGlobalListener listener = new AntlrGlobalListener();
         walker.walk(listener, programContext);
 
-        // TODO
-        // Properly get variables and names
-        // Determine scope
 	}
 
 }
 
 class AntlrGlobalListener extends MicroBaseListener {
+	private int blockCounter;
 
-    List strList = new ArrayList<String>();
+    public AntlrGlobalListener() {
+    	this.blockCounter = 1;
+    }
 
+
+    /** Scope stuff **/
     @Override
     public void enterPgm_body(MicroParser.Pgm_bodyContext ctx) {
-        System.out.println("Symbol table GLOBAL");
-    }
- 
-    @Override
-    public void enterString_decl(MicroParser.String_declContext ctx) {
-        // List<TerminalNode> tnodelist = getTokens()
-        System.out.println(ctx.getTokens(4)); // getTokens returns an array of all matches of ttype #
-        // System.out.println(ctx.getStart().getType());
+        System.out.println("GLOBAL");
+
     }
 
-    @Override
-    public void enterDecl(MicroParser.DeclContext ctx) {
-        // System.out.print(ctx.getText());
-        // System.out.println(ctx.getText());
+    @Override 
+    public void enterFunc_decl(MicroParser.Func_declContext ctx) { 
+    	// Create new symbol table with name ctx.getChild(2)
+    	System.out.println("\n" + ctx.getChild(2).getText());
+
     }
+
+    @Override 
+    public void enterIf_stmt(MicroParser.If_stmtContext ctx) { 
+    	// Create new symbol table with blockCounter
+    	System.out.println("\n" + blockCounter++);
+
+    }
+
+    @Override 
+    public void enterElse_part(MicroParser.Else_partContext ctx) { 
+    	// Create new symbol table with blockCounter
+    	if (!ctx.getText().isEmpty()) {
+	    	System.out.println("\n" + blockCounter++);
+    	}	
+
+
+    }
+
+    @Override 
+    public void enterDo_while_stmt(MicroParser.Do_while_stmtContext ctx) { 
+    	// Create new symbol table with blockCounter
+    	System.out.println("\n" + blockCounter++);
+    }
+ 
+
+
+    /** Variable declarations **/
+    @Override
+    public void enterString_decl(MicroParser.String_declContext ctx) {
+        System.out.print(ctx.getChild(1).getText() + " ");
+        System.out.println(ctx.getChild(3).getText());
+    }
+
+	@Override 
+	public void enterVar_decl(MicroParser.Var_declContext ctx) { 
+        System.out.println(ctx.getChild(0).getText() + " " + ctx.getChild(1).getText());
+        // Will need to parse beginning of variable to determine if int or float. Then will split on commas to get all variable names
+        // (this gross technique may be needed in order to acutally get all variables for declarations like INT a,b,c)
+
+    }
+
+	@Override 
+	public void enterParam_decl(MicroParser.Param_declContext ctx) { 
+        System.out.println(ctx.getChild(0).getText() + " " + ctx.getChild(1).getText());
+	}
+
+
+
+
+
+}
+
+class SymbolTable {
+	public void SymbolTable() {
+
+
+	}
 
 }
