0489d5c806b65312d580d240a754d91da1c7eb23
Brian Rieder <brieder@purdue.edu>
2016-10-18 22:59:15 -0400

    List of IR nodes class

---
+++ src/IRList.java
@@ -0,0 +1,27 @@
+// File name:   IRList.java
+// Updated:     19 October 2016
+// Authors:     Brian Rieder
+//              Kyle Rakos
+// Description: Intermediate representation node list
+
+import java.util.*;
+import java.lang.String;
+import java.io.*;
+
+public class IRList {
+    private List<IRNode> listIR = new ArrayList<IRNode>();
+
+    public IRNode getNode() {
+        return this.listIR.get(listIR.size()-1);
+    }
+    public IRNode getNode(int index) {
+        return this.listIR.get(index);
+    }
+
+    public void appendNode(IRNode node) {
+        this.listIR.add(node);
+    }
+    public void appendNode(String opcode, String operand1, String operand2, String result) {
+        this.listIR.add(IRNode(opcode, operand1, operand2, result));
+    }
+}
