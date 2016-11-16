82ea4e950c1b533794979aaf15dfbf4963268f8f
Brian Rieder <brieder@purdue.edu>
2016-11-15 23:53:32 -0500

    Added symbol table printing and searching for scope within a table

--- src/SymbolTable.java
+++ src/SymbolTable.java
@@ -17,11 +17,22 @@ class SymbolTable { // This symbol table contains a list of declarations
         this.objectList.add(obj);
     }
 
-    public void printSymbolTable() {
-            System.out.println("Symbol table " + this.scope);
-            for (int x = 0; x < this.objectList.size(); x++) {
-                this.objectList.get(x).print();
+    public String getScopeRegByVarName(String varName) {
+        String scopeReg = "";
+        for (int x = 0; x < this.objectList.size(); x++) {
+            if(this.objectList.get(x).varName.equals(varName)) {
+                scopeReg = this.objectList.get(x).scopeReg;
             }
         }
+        return scopeReg;
+    }
+
+    public void printSymbolTable() {
+        System.out.println("Symbol table " + this.scope);
+        for (int x = 0; x < this.objectList.size(); x++) {
+            this.objectList.get(x).print();
+        }
+        System.out.println();
+    }
 
 }
