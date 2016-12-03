a537735efa63929692a08513e39266fb313bdcf7
Brian Rieder <brieder@purdue.edu>
2016-12-02 23:53:42 -0500

    Created rudimentary node class, added print and append functions

---
+++ src/ControlFlowNode.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class ControlFlowNode {
+    private ArrayList<IRNode> statementList = new ArrayList<IRNode>();
+    private ArrayList<ControlFlowNode> adjacencyList = new ArrayList<ControlFlowNode>();
+
+    public ControlFlowNode(IRNode leader) {
+        this.statementList.add(leader);
+    }
+
+    public void appendStatement(IRNode inode) {
+        this.statementList.add(inode);
+    }
+
+    public ArrayList<IRNode> getStatementList() {
+        return this.statementList;
+    }
+
+    public void printControlFlowNode() {
+        System.out.println("Printing control flow node:");
+        for(IRNode statement : statementList) {
+            statement.printIRNode();
+        }
+    }
+}
