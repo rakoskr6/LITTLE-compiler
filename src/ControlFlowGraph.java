1d61790171d18cea2b58b35aaf72b139c5b45fe5
Brian Rieder <brieder@purdue.edu>
2016-12-03 00:46:03 -0500

    Generating nodes within CFG

--- src/ControlFlowGraph.java
+++ src/ControlFlowGraph.java
@@ -5,18 +5,36 @@ import java.io.*;
 class ControlFlowGraph {
     private HashMap<IRNode,ControlFlowNode> graph = new HashMap<IRNode,ControlFlowNode>();
 
-    public ControlFlowGraph(ArrayList<IRNode> worklist) {
-        generateNodes(worklist);
+    public ControlFlowGraph(ArrayList<IRNode> worklist, IRList irlist, int endOfFuncNum) {
+        generateNodes(worklist, irlist, endOfFuncNum);
         printControlFlowGraph();
     }
 
-    private void generateNodes(ArrayList<IRNode> worklist) {
-        while(worklist.size() > 0) {
+    private void generateNodes(ArrayList<IRNode> worklist, IRList irlist, int endOfFuncNum) {
+        while(worklist.size() > 1) {
             IRNode leaderNode = worklist.remove(0);
+            IRNode nextLeader = worklist.get(0);
             ControlFlowNode cfnode = new ControlFlowNode(leaderNode);
-            // add in the for loop from lecture 10, pg 9
+            ArrayList<IRNode> statementList = createStatementList(leaderNode, nextLeader, irlist);
+            cfnode.setStatementList(statementList);
             graph.put(leaderNode, cfnode);
         }
+        for(IRNode leaderNode : worklist) { // who the fuck knows why worklist.remove(0) throws an exception
+            ControlFlowNode cfnode = new ControlFlowNode(leaderNode);
+            ArrayList<IRNode> statementList = new ArrayList<IRNode>();
+            for(int i = leaderNode.getStatementNum(); i < endOfFuncNum; ++i) {
+                statementList.add(irlist.getNode(i - 1));
+            }
+            cfnode.setStatementList(statementList);
+            graph.put(leaderNode, cfnode);
+        }
+    }
+    private ArrayList<IRNode> createStatementList(IRNode leaderNode, IRNode nextLeader, IRList irlist) {
+        ArrayList<IRNode> statementList = new ArrayList<IRNode>();
+        for(int i = leaderNode.getStatementNum(); i < nextLeader.getStatementNum(); ++i) {
+            statementList.add(irlist.getNode(i - 1));
+        }
+        return statementList;
     }
 
     public void printControlFlowGraph() {
