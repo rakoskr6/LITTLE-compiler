a6584890d38b29d6ed9dad4094d68e788185b0d1
Brian Rieder <brieder@purdue.edu>
2016-12-03 17:06:37 -0500

    Leader and last getters, adjacency appends, and edge printing

--- src/ControlFlowNode.java
+++ src/ControlFlowNode.java
@@ -13,19 +13,35 @@ class ControlFlowNode {
     public void appendStatement(IRNode inode) {
         this.statementList.add(inode);
     }
+    public void appendAdjacency(ControlFlowNode cfn) {
+        this.adjacencyList.add(cfn);
+    }
 
     public ArrayList<IRNode> getStatementList() {
         return this.statementList;
+    }
+    public IRNode getLeaderStatement() {
+        return this.statementList.get(0);
+    }
+    public IRNode getLastStatement() {
+        return this.statementList.get(this.statementList.size()-1);
     }
 
     public void setStatementList(ArrayList<IRNode> slist) {
         this.statementList = slist;
     }
 
-    public void printControlFlowNode() {
+    public void printControlFlowNode(boolean printEdges) {
         System.out.println("Printing control flow node:");
         for(IRNode statement : statementList) {
             statement.printIRNode();
         }
+        if(printEdges) {
+            System.out.println("Printing leaders of edges:");
+            for(ControlFlowNode cfn : adjacencyList) {
+                cfn.getLeaderStatement().printIRNode();
+            }
+        }
+        System.out.println();
     }
 }
